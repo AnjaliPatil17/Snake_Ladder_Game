016f342795d988755f90782c13231e8423aa4a43
Anjali <anjaliap1227@gmail.com>
2024-05-08 09:56:05 +0530

    Snake_ladder_UC1-Single_player_at_start_position-0

--- Snake_Ladder_Game.java
+++ Snake_Ladder_Game.java
@@ -2,6 +2,9 @@ package com.BridgeLabez.Snake_Ladder;
 
 public class Snake_Ladder_Game {
     public static void main(String[] args) {
+        int diceRoll = 0;
+        int current_position = 0;
+        int max_position = 100;
 
         System.out.println("Welcome to Snake and Ladder Game!");
     }
