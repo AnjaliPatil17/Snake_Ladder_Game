d45fa5d5da86b879376438ce1dbd4c96e38fa811
Anjali <anjaliap1227@gmail.com>
2024-05-08 09:46:03 +0530

    Snake Ladder game

---
+++ Snake_Ladder_Game.java
@@ -0,0 +1,8 @@
+package com.BridgeLabez.Snake_Ladder;
+
+public class Snake_Ladder_Game {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake and Ladder Game!");
+    }
+}
