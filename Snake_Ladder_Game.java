e3acd4c37712f42d1bf9d3f8e1b6d88d9ea2255a
Anjali <anjaliap1227@gmail.com>
2024-05-08 12:31:28 +0530

    Snake_Ladder_UC-4_Player_Reached_Winning_Position

--- Snake_Ladder_Game.java
+++ Snake_Ladder_Game.java
@@ -5,14 +5,14 @@ public class Snake_Ladder_Game {
         int diceRoll = 0;
         int current_position = 1;
         int max_position = 100;
-        int Snake_Array[]={6,12,26,35,42,67,88,97};
-        int Ladder_Array[]={3,15,36,49,55,77,82,95};
+        int Snake_Array[]={6,12,26,35,42,67,88,92};
+        int Ladder_Array[]={3,15,36,49,55,77,82,90};
 
         System.out.println("Welcome to Snake and Ladder Game!");
         System.out.println("Starting Position of the Player = 0");
 
-        for(current_position=1;current_position <= 100;current_position++) {
-            diceRoll = (int) ((Math.random() * 6) + 1);
+        for(current_position=1;current_position < 100;current_position++) {
+            diceRoll = (int) ((Math.random() * 6) );
             System.out.println("Dice Number :" + diceRoll);
 
             current_position = current_position +diceRoll;
@@ -106,6 +106,14 @@ public class Snake_Ladder_Game {
                 current_position -= diceRoll;
                 System.out.println("You got a snake.");
                 System.out.println("You at position: "+current_position);
+            }
+
+            if(current_position <0)
+                current_position = 0;
+
+
+            if(current_position == max_position){
+                System.out.println("Congratulations, You win the game!");
             }
 
         }
